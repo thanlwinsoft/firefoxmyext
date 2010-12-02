2092de38ec14f9ea347a106c6bc9d33c004ab862
PhyoHtetHlaing <phyohtet@thanlwinsoft.org>
2010-12-02 11:49:10 +0630

    Update docs

--- firefox-myanmar-converter/docs/index.php
+++ firefox-myanmar-converter/docs/index.php
@@ -61,7 +61,7 @@ This extension automatically detects if a web page is using Zaw Gyi One fonts an
 This means that you only need to install a Unicode font, you don't need to have ZawGyi installed.
 </p>
 <p lang="my" class="myTranslation">
-web page တစ်ခုက စကားလုံးများကို ဇော်ဂျီဖောင့်ဖြင့် အသုံးပြုရေးသားထားလျှင် ဤတိုးချဲ့အထူးပြုချက်က ယူနီကုတ်သို့ အလိုအ​​လျောက် ရှာဖွေပြောင်းလဲပေးနိုင်သည်။ ထိုကြောင့် သင်သည် ယူနီကုတ်ဖောင့်ကိုသာ ထည့်သွင်းရန်လိုအပ်ပြီး၊ ဇော်ဂျီဖောင့်ကို ထည့်သွင်းရန်လုံးဝမလိုအပ်ပါဟုဆိုလိုခြင်းပင်ဖြစ်သည်။
+web page တစ်ခုက စကားလုံးများကို ဇော်ဂျီဖောင့်ဖြင့် အသုံးပြုရေးသားထားလျှင် ဤတိုးချဲ့အထူးပြုချက်က ယူနီကုတ်သို့ အလိုအ​​လျောက် ရှာဖွေပြောင်းလဲပေးနိုင်သည်။ ထိုကြောင့် သင်သည် ယူနီကုတ်ဖောင့် တစ်မျိုးတည်းကိုသာ ထည့်သွင်းရန်လိုအပ်ပြီး၊ ဇော်ဂျီဖောင့်ကို ထည့်သွင်းရန် မလိုအပ်ပါဟု ဆိုလိုခြင်းပင်ဖြစ်သည်။
 </p>
 
 <h2 class="enTranslation">Requirements</h2>
@@ -79,7 +79,7 @@ If the wrong font is selected, then the Myanmar text will display wrongly.
 Unicode has many advantages over Zaw Gyi:
 </p>
 <p lang="my" class="myTranslation">
-ဇော်ဂျီဖောင့်နှင့်ယူနီကုတ်ဖောင့် နှစ်ခုလုံးရှိနေသောအခါ web page အနေဖြင့် မည်သည့်ဖောင့်ဆိုသည်ကို ပြတ်သားစွာ မဖော်ပြနိုင်တဲ့ ပြဿနာများကို ဖြစ်စေနိုင်ပါသည်။ မှားယွင်းတဲ့ ဖောင့်ကို​ရွေးချယ်မိခဲ့မယ်ဆိုလျှင်ဖြင့် မြန်မာစာလုံးများသည်လည်း မှန်ကန်စွာ ဖော်ပြနိုင်တော့မည်မဟုတ်ပေ။ ယူနီကုတ်တွင် ဇော်ဂျီထက်သာလွန်သော အကျိုးကျေးဇူးများရှိပေသည်။
+web page အနေဖြင့် မည်သည့်ဖောင့်ဆိုသည်ကို ပြတ်သားစွာ မဖော်ပြနိုင်သောအခါ ဇော်ဂျီဖောင့် နှင့် ယူနီကုတ်ဖောင့် နှစ်မျိုးလုံးရှိနေခြင်းသည် ပြဿနာများကို ဖြစ်စေပါသည်။ မှားယွင်းသောဖောင့်ကို​ ရွေးချယ်မိခဲ့မည်ဆိုလျှင်ဖြင့် မြန်မာစာလုံးများ သည်လည်း မှားယွင်းစွာ ဖော်ပြနေပါလိမ့်မည်။ ယူနီကုတ်တွင် ဇော်ဂျီထက်သာလွန်သော အကျိုးကျေးဇူးများရှိပေသည်။
 </p>
 <ul class="enTranslation">
 <li>Unicode has only one spelling for a Myanmar word, Zaw Gyi can have multiple spellings for the same word.</li>
@@ -90,8 +90,8 @@ Unicode has many advantages over Zaw Gyi:
 </ul>
 <ul lang="my" class="myTranslation">
 <li>မြန်မာစကားလုံး တစ်လုံးအတွက် ယူနီကုတ်တွင် စာလုံးပေါင်းပုံ တစ်မျိုးတည်းသာ ရှိပါသည်။ တူညီတဲ့ စကားလုံးအတွက်ဇော်ဂျီတွင် စာလုံးပေါင်းပုံများ အများအပြား ရှိနိုင်ပါသည်။</li>
-<li>ယူနီကုတ်ဖြင့် အမျိုးအစားခွဲခြင်း၊ ရှာဖွေခြင်းတွေကို လွယ်ကူစွာလုပ်​ဆောင်နိုင်ပါသည်။</li>
-<li>အများနှင့်ဆိုင်သောအဆင့်မှီဆော့ဝဲလ်များတွင် ယူနီကုတ်ကိုသာ အသုံးပြုပါလိမ့်မည်။ </li>
+<li>ယူနီကုတ်ဖြင့် စကားလုံး အမျိုးအစားခွဲခြင်း၊ ရှာဖွေခြင်းများကို ပိုမိုလွယ်ကူစွာလုပ်​ဆောင်နိုင်ပါသည်။</li>
+<li>အများနှင့်ဆိုင်သောအဆင့်မှီဆော့ဝဲလ်များသည် ယူနီကုတ်ကိုသာ အသုံးပြုပါလိမ့်မည်။ </li>
 <li>ယူနီကုတ်ဖြင့် မြန်မာစာ စာလုံးပေါင်းသတ်ပုံ စစ်ဆေးခြင်းများ ဖြစ်လာနိုင်ဖွယ်ရှိပါသည်။</li>
 <li>ယူနီကုတ်ဖြင့် မွန်၊ ကရင်၊ ရှမ်း စကားလုံးများကို မှန်ကန်စွာ ပြသနိုင်ပါသည်။</li>
 </ul>
@@ -104,16 +104,16 @@ $dir->listDir();
 <h2 class="enTranslation">Usage</h2>
 <h2 lang="my" class="myTranslation">အသုံးပြုပုံ</h2>
 <p class="enTranslation">
-Many web pages are using ZawGyi-One or other fonts which are not following the Unicode Standard correctly. The "Myanmar Converter" Firefox Extension allows people with only genuine Myanmar Unicode fonts installed to read these pages by converting the text to Unicode when the page opens.
+Many Myanmar web pages are using ZawGyi or other fonts which are not following the Unicode Standard correctly. The "Myanmar Converter" Firefox Extension allows people with only genuine Myanmar Unicode fonts installed to read these pages by converting the text to Unicode when the page opens.
 </p>
 <p lang="my" class="myTranslation">
-web page ​တော်တော်များများသည် ယူနီကုတ်အဆင့်အတန်းကို မှန်ကန်စွာမလိုက်နာသော ဇော်ဂျီ သို့မဟုတ် အခြားဖောင့် များကို အသုံးပြုနေကြပါသည်။ ဤစာမျက်နှာများကို ဖတ်ရှုနိုင်ရန် စစ်မှန်သော မြန်မာယူနီကုတ်ဖောင့် တစ်မျိုးတည်းကိုသာ ထည့်သွင်းအသုံးပြုထားသော သူများအနေဖြင့် စာမျက်နှာဖွင့်လိုက်သောအခါ Firefox ကိုတိုးချဲ့အထူးပြုထားသော မြန်မာစာပြောင်းလဲခြင်း (The "Myanmar Converter" Firefox Extension) သည် စာသားများကို ယူနီကုတ်သို့ ပြောင်းလဲပေးနိုင်ပါသည်။
+မြန်မာ web page ​တော်တော်များများသည် ယူနီကုတ်အဆင့်အတန်းကို မှန်ကန်စွာမလိုက်နာသော ဇော်ဂျီ သို့မဟုတ် အခြားဖောင့် များကို အသုံးပြုနေကြပါသည်။ စစ်မှန်သော မြန်မာယူနီကုတ်ဖောင့် တစ်မျိုးတည်းကိုသာ ထည့်သွင်းအသုံးပြုထားသော သူများအနေဖြင့် ထိုစာမျက်နှာများကို ဖတ်ရှုနိုင်ရန် စာမျက်နှာဖွင့်လိုက်သောအခါ Firefox ကို တိုးချဲ့ အထူးပြုထားသော မြန်မာစာပြောင်းလဲခြင်း (The "Myanmar Converter" Firefox Extension) ၏ စာသားများကို ယူနီကုတ်သို့ ပြောင်းလဲပေးခြင်းအားဖြင့် ဖတ်ရှုနိုင်ပါသည်။
 </p>
 <p class="enTranslation">
 The Myanmar text will display wrongly like this:
 </p>
 <p lang="my" class="myTranslation">
-မြန်မာစာလုံးတွေဟာ ဤကဲ့သို့ မှားယွင်းစွာဖော်ပြနေပါသည်-
+မြန်မာစာလုံးများသည် ဤကဲ့သို့ မှားယွင်းစွာဖော်ပြနေပါမည်-
 <p class="centre">
 <img src="images/beforeConversion.png" />
 </p>
@@ -124,7 +124,7 @@ The Myanmar text will display wrongly like this:
 First, we must select the Tools,Myanmar Converter on toolbar.
 </p>
 <p lang="my" class="myTranslation">
-ပထမဆုံး​အနေဖြင့် ကျွန်တော်တို့ဟာ toolbar(ကိရိယာတန်ဆာပလာဘား) ပေါ်က Tools ဆိုတဲ့ ကိရိယာထဲက Myanmar Converter ဆိုတဲ့ အရာကိုရွေးရပါမည်။ 
+ပထမဆုံး​အနေဖြင့် ကျွန်တော်တို့သည် toolbar(ကိရိယာတန်ဆာပလာဘား) ပေါ်က Tools ဆိုသည့် ကိရိယာထဲမှ Myanmar Converter ဆိုသည့် အရာကို ရွေးချယ်ရပါမည်။ 
 </p>
 <p class="centre">
 <img src="images/state1.png" alt="Ubuntu Software Center/Myanmar"/>
@@ -136,7 +136,7 @@ First, we must select the Tools,Myanmar Converter on toolbar.
 The Myanmar Converter Options box is appeared and we can choose match exactly or match as suffix for Hostname and match exactly or match as prefix for Partname.And we must check Enable conversion for Pattern check box and click the Add button.
 </p>
 <p lang="my" class="myTranslation">
-မြန်မာစာဖောင့်ပြောင်းလဲရာ၌ ရွေးချယ်ပိုင်ခွင့်များ (Myanmar Converter Options) ဆိုတဲ့ boxတစ်ခု ပေါ်လာပါမည်၊ ပြီးနောက်ကျွန်တော်တို့ဟာ အဓိကဆာလ်ဗာအမည် (Hostname) အတွက် အားလုံးကိုတိတိကျကျတွဲပါ (match exactyl) သို့မဟုတ် ​ရှေ့မှစ၍အားလုံးကိုတွဲပါ (match as suffix) ကိုရွေးချယ်ရပြီး လမ်းကြောင်းခွဲအမည် (Partname) အတွက်လည်း အားလုံးကိုတိတိကျကျတွဲပါ (match exactyl) သို့မဟုတ် အနောက်မှစ၍အားလုံးကိုတွဲပါ (match as prefix) ကိုရွေးချယ်ရပါမည်။ ထို့နောက် ကျွန်တော်တို့ဟာ ပုံစံအလိုက်ပြောင်းလဲ​နိုင်သောစနစ် (Enable conversion for Pattern) ဆိုတဲ့ check boxလေးကို အမှန်ပေးပြီး Add ဆိုတဲ့ ခလုပ်ကိုနှိပ်လိုက်ရပါမည်။</p>
+မြန်မာစာဖောင့်ပြောင်းလဲရာ၌ ရွေးချယ်ပိုင်ခွင့်များ (Myanmar Converter Options) ဆိုသည့် box တစ်ခု ပေါ်လာပါမည်၊ ပြီးနောက်ကျွန်တော်တို့သည် အဓိကဆာလ်ဗာအမည် (Hostname) အတွက် အားလုံးကိုတိတိကျကျတွဲပါ (match exactyl) သို့မဟုတ် ​ရှေ့မှစ၍အားလုံးကိုတွဲပါ (match as suffix) ကိုရွေးချယ်ရပြီး လမ်းကြောင်းခွဲအမည် (Partname) အတွက်လည်း အားလုံး ကိုတိတိကျကျတွဲပါ (match exactyl) သို့မဟုတ် အနောက်မှစ၍အားလုံးကိုတွဲပါ (match as prefix) ကို ရွေးချယ်ရပါမည်။ ထို့နောက် ကျွန်တော်တို့သည် ပုံစံအလိုက်ပြောင်းလဲ​နိုင်သောစနစ် (Enable conversion for Pattern) ဆိုသည့် check box လေးကို အမှန်ပေးပြီး Add ဆိုသည့် ခလုပ်ကိုနှိပ်လိုက်ရပါမည်။</p>
 <p  class="centre">
 <img src="images/state2.png" />
 </p>
@@ -144,7 +144,7 @@ The Myanmar Converter Options box is appeared and we can choose match exactly or
 <br></br>
 
 <p class="enTranslation">After above states we can click the Ok button.</p>
-<p lang="my" class="myTranslation">အထက်ပါအချက်များပြည့်စုံပြီဆိုလျှင်တော့ OK ဆိုတဲ့ခလုပ်ကိုနှိပ်နိုင်ပါပြီ။</p> 
+<p lang="my" class="myTranslation">အထက်ပါအချက်များပြည့်စုံပြီဆိုလျှင်ဖြင့် OK ဆိုသည့်ခလုပ်ကို နှိပ်နိုင်ပါပြီ။</p> 
 <p  class="centre">
 <img src="images/state3.png" />
 </p>
@@ -152,7 +152,7 @@ The Myanmar Converter Options box is appeared and we can choose match exactly or
 <br></br>
 
 <p class="enTranslation">And reload the web page, the Myanmar text will display correctly.</p>
-<p lang="my" class="myTranslation">ထိုနောက် web page ကို reload ပြန်လုပ်ခိုင်းပြီးလျှင်တော့ မြန်မာစာလုံးတွေဟာ တိကျမှန်ကန်စွာ ထွက်ပေါ်လာပါလိမ့်မည်။</p>
+<p lang="my" class="myTranslation">ထိုနောက် web page ကို reload ပြန်လုပ်ခိုင်းပြီးလျှင်ဖြင့် မြန်မာစာလုံးများသည် တိကျမှန်ကန်စွာ ထွက်ပေါ်လာပါလိမ့်မည်။</p>
 <p  class="centre">
 <img src="images/afterConversion.png" />
 </p>
@@ -163,7 +163,7 @@ The Myanmar Converter Options box is appeared and we can choose match exactly or
 If you want to send Myanmar text to another person with Zawgyi or other fonts, select the textarea and right click on mouse and then choose Myanmar Unicode Converter, Send form as: ,Zawgyi-One or as you like.You can send from Unicode to other fonts who can not use Unicode by using this states.
 </p>
 <p lang="my" class="myTranslation">
-သင်ဟာ မြန်မာစာလုံးများကို ဇော်ဂျီ သို့မဟုတ် အခြားဖောင့်များဖြင့် အခြားသူတစ်ဦးကို ပေးပို့ချင်ရင်တော့၊ ​ပေးပို့ချင်တဲ့ textarea ကိုရွေးလိုက်ပါ။ ထို့နောက် မောက်စ်ပေါ်မှာညာဘက်ကိုတချက်ကလစ်လိုက်ပါ၊ ပြီး​လျှင်တော့ မြန်မာယူနီကုတ်ပြောင်းလဲခြင်း (Myanmar Unicode Converter) ထဲက ဤပုံစံဖြင့်ပို့ပါ- (Send form as:) ကိုရွေးပါ၊ ထို့နောက်ဇော်ဂျီ သို့မဟုတ် မိမိပို့ချင်သောဖောင့်ကို ရွေးလိုက်ပါ။ ဤပုံစံကိုအသုံးပြုခြင်းဖြင့် သင်ဟာ ယူနီကုတ်အသုံးမပြုနိုင်သေးသော သူများကို ယူနီကုတ်မှတဆင့် အခြားဖောင့်များသို့ ပြောင်းလည်းဆက်သွယ်၍ ရနိုင်ပါသည်။
+သင်သည် မြန်မာစကားလုံးများကို ဇော်ဂျီ သို့မဟုတ် အခြားဖောင့်များဖြင့် အခြားသူတစ်ဦးကို ပေးပို့ချင်လျှင်ဖြင့်၊ ​ပေးပို့ချင်သည့် စာသားပါရှိသော textarea ပေါ်ကိုမောက်စ်​တင်လိုက်ပါ။ ထို့နောက် မောက်စ်ညာဘက်ကို တချက်ကလစ်လိုက်ပါ၊ ပြီး​လျှင်ဖြင့် မြန်မာယူနီကုတ်ပြောင်းလဲခြင်း (Myanmar Unicode Converter) ထဲမှ ဤပုံစံဖြင့်ပို့ပါ- (Send form as:) ကိုရွေးပါ၊ ထို့နောက်ဇော်ဂျီ သို့မဟုတ် မိမိပို့ချင်သောဖောင့်ကို ရွေးလိုက်ပါ။ ဤပုံစံကိုအသုံးပြုခြင်းဖြင့် သင်ဟာ ယူနီကုတ်အသုံးမပြုနိုင်သေးသော သူများကို ယူနီကုတ်မှတဆင့် အခြားဖောင့်များသို့ ပြောင်းလည်းဆက်သွယ်၍ ရနိုင်ပါသည်။
 </p>
 <p  class="centre">
 <img src="images/gmail.png" />
